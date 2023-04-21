14eaca9cb0a60b4c0f2f48d054a01deac9f7fb4b
Cheder-one <cherbadenis1@gmail.com>
2023-04-21 23:22:04 +0300

    u

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,10 @@
 import Home from "./components/navigation/home";
 import Login from "./components/navigation/login";
 import NavBar from "./components/navigation/navBar";
-import { Route, Switch } from "react-router-dom";
+import { Redirect, Route, Switch } from "react-router-dom";
 import Posts from "./components/navigation/posts";
 import Contacts from "./components/navigation/contacts";
+import NotFound from "./components/redirect/notFound";
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
           <Route path="/login" component={Login} />
           <Route path="/posts/:postId?" component={Posts} />
           <Route path="/contacts" component={Contacts} />
+          <Redirect from="/lk" to="/login" />
+          <Route component={NotFound} />
         </Switch>
       </div>
     </>
